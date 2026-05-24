aa91a8d9187d6c4feb04b03366d5ae2fa8ba71dc
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-24 20:22:19 +0300

    feat(mai): copy split from S3

--- alekseev.ivan/S4/main.cpp
+++ alekseev.ivan/S4/main.cpp
@@ -1,10 +1,12 @@
 #include "BSTree.h"
+#include "../common/vector.h"
 #include <iostream>
 #include <fstream>
 
 namespace alekseev {
   using big_tree_t = BSTree< std::string, BSTree< int, std::string, std::less< > >, std::less< > >;
   std::ifstream & input_dicts(std::ifstream & is, big_tree_t bigTree);
+  Vector< std::string > split(const std::string & s, char delim = ' ');
 
   void print(big_tree_t bigTree, std::string args);
   void complement(big_tree_t bigTree, std::string args);
@@ -20,3 +22,20 @@ namespace alekseev {
 }
 
 int main();
+
+alekseev::Vector< std::string > alekseev::split(const std::string & s, char delim)
+{
+  size_t start = 0;
+  Vector< std::string > res;
+  size_t i = 0;
+  for (; i < s.size(); ++i) {
+    if (s[i] == delim) {
+      if (i > start) {
+        res.pushBack(s.substr(start, i - start));
+      }
+      start = i + 1;
+    }
+  }
+  res.pushBack(s.substr(start, i - start));
+  return res;
+}
