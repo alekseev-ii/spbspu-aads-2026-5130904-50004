ab1b63b82289fe2991028527d9616e9899115109
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-26 19:32:30 +0300

    feat(dictionary): add basic declaration

--- alekseev.ivan/F0/dictionary.h
+++ alekseev.ivan/F0/dictionary.h
@@ -1,7 +1,9 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
+
 #include <string>
 #include "../common/vector.h"
+#include "cuckoo_hash.h"
 
 namespace alekseev {
   enum pos {
@@ -47,6 +49,24 @@ namespace alekseev {
     std::string lemma_;
     Vector< WordForm > forms_;
     pos default_pos;
+  };
+
+  using str_cr = const std::string &;
+  size_t djb2_hash(str_cr line);
+  size_t poly_hash(str_cr line);
+  bool equal(str_cr s1, str_cr s2);
+
+  struct Dictionary {
+    ~Dictionary();
+    Dictionary(const Dictionary &);
+    Dictionary & operator=(const Dictionary &);
+    Dictionary(Dictionary &&) noexcept;
+    Dictionary & operator=(Dictionary &&) noexcept;
+    Dictionary(str_cr file_name);
+
+    private:
+      CuckooHash< std::string, Lemma, size_t (*)(str_cr), size_t (*)(str_cr), bool(*)(str_cr,
+          str_cr) > lemmas_;
   };
 }
 
