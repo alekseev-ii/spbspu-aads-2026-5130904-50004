61e4081a95e340936970c0956654b57a30af3b7b
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-26 19:36:30 +0300

    feat(dictionary): add hash functions

--- alekseev.ivan/F0/dictionary.cpp
+++ alekseev.ivan/F0/dictionary.cpp
@@ -1,1 +1,25 @@
 #include "dictionary.h"
+
+size_t alekseev::djb2_hash(str_cr line)
+{
+  size_t hash = 5381;
+  for (size_t i = 0; i < line.size(); ++i) {
+    hash = ((hash << 5) + hash) + line[i];
+  }
+  return hash;
+}
+
+size_t alekseev::poly_hash(str_cr line)
+{
+  size_t hash = 0;
+  int p = 67;
+  for (size_t i = 0; i < line.size(); ++i) {
+    hash = hash * p + line[i];
+  }
+  return hash;
+}
+
+bool alekseev::equal(str_cr s1, str_cr s2)
+{
+  return s1 == s2;
+}
