47b86fd536d5706cae35c379aa27e3a1e3f502f2
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-26 19:13:09 +0300

    feat(dictionary): add basic structures

---
+++ alekseev.ivan/F0/dictionary.h
@@ -0,0 +1,53 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+#include <string>
+#include "../common/vector.h"
+
+namespace alekseev {
+  enum pos {
+    noun, verb, adj
+  };
+
+  enum gender {
+    nn_gender, female, male, neuter
+  };
+
+  enum number {
+    nn_count, singular, plural
+  };
+
+  enum case_ {
+    nn_case, nom, gen, dat, acc, ins, pre
+  };
+
+  enum tense {
+    nn_tense, past, present, future
+  };
+
+  enum person {
+    nn_person, fst, scd, thd
+  };
+
+  enum aspect {
+    nn_aspect, perf, imperf
+  };
+
+  struct WordForm {
+    std::string word_;
+    pos pos_;
+    gender gender_;
+    number number_;
+    case_ case_;
+    tense tense_;
+    person person_;
+    aspect aspect_;
+  };
+
+  struct Lemma {
+    std::string lemma_;
+    Vector< WordForm > forms_;
+    pos default_pos;
+  };
+}
+
+#endif
