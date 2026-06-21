afb5ac69928e1e1108a8948d9e78e0ea7769bf99
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-06-21 12:41:22 +0300

    fix(S1): small fixes......

--- alekseev.ivan/S1/main.cpp
+++ alekseev.ivan/S1/main.cpp
@@ -3,8 +3,6 @@
 #include "List.h"
 
 namespace alekseev {
-  template< class T >
-  bool is_empty(const List< List< T > > & l);
   const size_t MAX_SIZE_T = std::numeric_limits< size_t >::max();
   template< class FwdIter >
   std::ostream & print(std::ostream & os, FwdIter beg, FwdIter end);
@@ -16,7 +14,6 @@ int main()
   auto cur_name = names.before_begin();
   alekseev::List< alekseev::List< size_t > > seqs;
   auto cur_seq = seqs.before_begin();
-  alekseev::List< size_t > sums;
 
   std::string name;
   while (std::cin >> name) {
@@ -25,23 +22,13 @@ int main()
     seqs.insert_after(cur_seq, alekseev::List< size_t >());
     ++cur_seq;
     auto current = cur_seq->before_begin();
-    auto cur_sum = sums.before_begin();
 
     int next_char = std::cin.get();
     while (std::cin && next_char != '\n' && next_char != EOF) {
       size_t n = 0;
       std::cin >> n;
-      if (sums.size() == cur_seq->size()) {
-        sums.insert_after(cur_sum, 0ull);
-      }
-      ++cur_sum;
       cur_seq->insert_after(current, n);
       ++current;
-      if (alekseev::MAX_SIZE_T - n < *cur_sum) {
-        std::cerr << "Overflow!\n";
-        return 1;
-      }
-      *cur_sum += n;
       next_char = std::cin.get();
     }
   }
@@ -52,39 +39,45 @@ int main()
   }
   alekseev::print(std::cout, names.begin(), names.end());
 
+  alekseev::List< size_t > sums;
+  auto cur_sum = sums.before_begin();
+  alekseev::List< alekseev::List< size_t > > bufs;
+  auto last_buf = bufs.before_begin();
   while (true) {
-    alekseev::List< size_t > buffer;
-    auto last = buffer.before_begin();
+    bufs.insert_after(last_buf, alekseev::List< size_t >());
+    ++last_buf;
+    auto last = last_buf->before_begin();
+
     for (auto seq = seqs.begin(); seq != seqs.end(); ++seq) {
       if (!seq->empty()) {
+        if (last_buf->empty()) {
+          sums.insert_after(cur_sum, 0ull);
+          ++cur_sum;
+        }
         size_t n = seq->front();
-        buffer.insert_after(last, n);
+        if (alekseev::MAX_SIZE_T - n < *cur_sum) {
+          std::cerr << "Overflow!\n";
+          return 1;
+        }
+        *cur_sum += n;
+        last_buf->insert_after(last, n);
         ++last;
         seq->pop_front();
       }
     }
-    if (buffer.empty()) {
+    if (last_buf->empty()) {
       break;
     }
-    alekseev::print(std::cout, buffer.begin(), buffer.end());
   }
 
+  for (auto buf = bufs.begin(); buf != bufs.end(); ++buf) {
+    alekseev::print(std::cout, buf->begin(), buf->end());
+  }
   if (sums.empty()) {
     std::cout << "0\n";
   } else {
     alekseev::print(std::cout, sums.begin(), sums.end());
   }
-}
-
-template< class T >
-bool alekseev::is_empty(const List< List< T > > & l)
-{
-  for (auto it = l.begin(); it != l.end(); ++it) {
-    if (!it->empty()) {
-      return false;
-    }
-  }
-  return true;
 }
 
 template< class FwdIter >
