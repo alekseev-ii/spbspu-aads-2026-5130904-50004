3f5a5488f48bcbb1bce8128d1354f88862bf200b
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-06-22 11:01:10 +0300

    refactor(stack): refactor with new List

--- alekseev.ivan/common/stack.h
+++ alekseev.ivan/common/stack.h
@@ -2,21 +2,17 @@
 #define STACK_H
 
 #include "List.h"
-#include <cstddef>
-#include <cassert>
 
 namespace alekseev {
   template< class T >
-  struct Stack {
-    List< T > * data_;
-    size_t size_;
-
+  struct Stack
+  {
     Stack();
-    ~Stack();
-    Stack(const Stack & rhs);
-    Stack & operator=(const Stack & rhs);
-    Stack(Stack && rhs) noexcept;
-    Stack & operator=(Stack && rhs) noexcept;
+    ~Stack() = default;
+    Stack(const Stack & rhs) = default;
+    Stack & operator=(const Stack & rhs) = default;
+    Stack(Stack && rhs) noexcept = default;
+    Stack & operator=(Stack && rhs) noexcept = default;
 
     template< class U >
     void push(U && value);
@@ -25,120 +21,45 @@ namespace alekseev {
     T & top();
     bool empty() const;
     size_t size() const;
+
+    private:
+      List< T > stack_;
   };
 
   template< class T >
   Stack< T >::Stack():
-    data_(fake< T >()),
-    size_(0)
-  {
-  }
-
-  template< class T >
-  Stack< T >::~Stack()
-  {
-    clear(data_->next, data_);
-    rmfake(data_);
-  }
-
-  template< class T >
-  Stack< T >::Stack(const Stack & rhs):
-    data_(fake< T >()),
-    size_(0)
-  {
-    List< T > * current = rhs.data_->next;
-    for (size_t i = 0; i < rhs.size(); ++i) {
-      try {
-        push(current->data);
-      } catch (...) {
-        clear(data_->next, data_);
-        rmfake(data_);
-        throw;
-      }
-      current = current->next;
-    }
-  }
-
-  template< class T >
-  Stack< T > & Stack< T >::operator=(const Stack & rhs)
-  {
-    if (this == &rhs) {
-      return *this;
-    }
-    List< T > * lhs_fake_node = fake< T >();
-    List< T > * lhs_current = lhs_fake_node;
-    List< T > * rhs_current = rhs.data_->next;
-    for (size_t i = 0; i < rhs.size(); ++i) {
-      try {
-        lhs_current = insert_after(lhs_current, rhs_current->data);
-        rhs_current = rhs_current->next;
-      } catch (...) {
-        clear(lhs_fake_node->next, lhs_fake_node);
-        rmfake(lhs_fake_node);
-        throw;
-      }
-    }
-    clear(data_->next, data_);
-    rmfake(data_);
-    data_ = lhs_fake_node;
-    size_ = rhs.size();
-    return *this;
-  }
-
-  template< class T >
-  Stack< T >::Stack(Stack && rhs) noexcept:
-    data_(rhs.data_),
-    size_(rhs.size())
-  {
-    rhs.data_ = fake< T >();
-    rhs.size_ = 0;
-  }
-
-  template< class T >
-  Stack< T > & Stack< T >::operator=(Stack && rhs) noexcept
-  {
-    clear(data_->next, data_);
-    rmfake(data_);
-    data_ = rhs.data_;
-    size_ = rhs.size();
-    rhs.data_ = fake< T >();
-    rhs.size_ = 0;
-    return *this;
-  }
+    stack_()
+  { }
 
   template< class T >
   template< class U >
   void Stack< T >::push(U && value)
   {
-    insert_after(data_, std::forward< U >(value));
-    ++size_;
+    stack_.push_front(std::forward< U >(value));
   }
 
   template< class T >
   void Stack< T >::pop()
   {
-    assert(!empty());
-    erase_after(data_);
-    --size_;
+    stack_.pop_front();
   }
 
   template< class T >
   T & Stack< T >::top()
   {
-    assert(!empty());
-    return data_->next->data;
+    return stack_.front();
   }
 
   template< class T >
   bool Stack< T >::empty() const
   {
-    return size_ == 0;
+    return stack_.empty();
   }
 
   template< class T >
   size_t Stack< T >::size() const
   {
-    return size_;
+    return stack_.size();
   }
 }
 
